778f18c232626b6ffe74016bbd16c2a2b8f2b9f9
stormchaser33 <nicolasthiry860@gmail.com>
2023-06-21 16:05:29 +0200

    init comimt

---
+++ dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
